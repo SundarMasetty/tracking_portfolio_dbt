469e6477aec03cac5ddfb771bf279341885bd3f7
saisundarmasetty.data@gmail.com <saisundarmasetty.data@gmail.com>
2026-02-25 15:52:59 +0000

    added v2 for stg and hashed CTE

---
+++ models/staging/v2_STG_UB_BANK_POSITION.sql
@@ -0,0 +1,32 @@
+-- we will be adding some meta data for this staging version and i will be adding record_source for the previous staging query and a cte
+-- RECORD_SOURCE is a metadata which we can able to find the source where it is derived from
+WITH 
+    src_data AS (
+        SELECT 
+            ACCOUNT_ID AS ACCOUNT_CODE,
+            SYMBOL AS SECURITY_CODE,
+            DESCRIPTION AS SECURITY_NAME,
+            EXCHANGE AS EXCHANGE_CODE,
+            REPORT_DATE AS REPORT_DATE,
+            QUANTITY AS QUANTITY,
+            COST_BASE AS COST_BASE,
+            POSITION_VALUE AS POSITION_VALUE,
+            CURRENCY AS CURRENCY_CODE,
+            'SOURCE_DATA.UB_BANK_POSITION' AS RECORD_SOURCE  --Here as we are having the CSV file and we loaded in to table we have hardcoded the value here.
+        FROM {{ source('ub_bank', 'UB_BANK_POSITION') }}
+    ),
+
+
+-- Now in this staging table i will be adding the hashed difference because we can track some field changes which can able to capture the data change
+
+hashed AS (
+        SELECT 
+            concat_ws('|' , ACCOUNT_CODE,SECURITY_CODE) AS POSITION_HKEY
+           , concat_ws('|' , ACCOUNT_CODE,SECURITY_CODE,SECURITY_NAME,EXCHANGE_CODE,REPORT_DATE,QUANTITY,COST_BASE,POSITION_VALUE,CURRENCY_CODE ) AS POSITION_HDIFF
+            , * 
+            , '{{ run_started_at }}' AS LOAD_TS_UTC --metadata loading ot this operation was performed time
+            FROM src_data
+)
+SELECT * FROM hashed
+
+
